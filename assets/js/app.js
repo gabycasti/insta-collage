a877a4c51580f6fdb90fed289046d2c34b1909f0
gabycasti <castillg@gmail.com>
2018-02-05 22:39:50 -0300

    validando datos con local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,52 @@
+$( document ).ready(function(){
+    
+    $("#sesion").click(function() {
+     let usuario = $("#usuario").val();
+     let datos = obtener_localstorage();
+     let name = $("#usuario").val();
+     console.log(name);
+     let pasword = $("#clave").val();
+     console.log(pasword);
+     console.log(datos.clave);
+     if (name == datos.usuario && pasword == datos.clave){
+
+        document.location.replace('collage.html')
+     }else if(usuario == ""){
+       $("#mensaje_usuario").fadeIn();
+       return false;
+     }else{
+        $('#mensaje_usuario').fadeOut();
+         return false;
+     }
+       
+    })
+
+
+});
+
+
 guardar_localstorage();
 
-//obtener_localstorage();
 
 function obtener_localstorage(){
 
     if(localStorage.getItem("nombre")){
-    let nombre = localStorage.getItem("nombre");
+    //let nombre = localStorage.getItem("nombre");
     let persona = JSON.parse(localStorage.getItem("persona"));
-    console.log(nombre);
-    console.log(persona);
-
+    return persona;
     }else
     console.log("no hay entradas")
-
-}
+};
 
 
 
 
 function guardar_localstorage(){
-
-
     let persona = {
         usuario: "gabycasti",
-        clave: 456789
+        clave: "4567"
     }
-
-    let nombre = "pedro";
-
-    localStorage.setItem("nombre",nombre);
+    //localStorage.setItem("nombre",nombre);
     localStorage.setItem("persona",JSON.stringify(persona));
 }
 
